d9a281c4252d0b81f3d8f0862727e265643a2290
Alex S Grimes <alex@alexgrimes.com>
2020-03-27 20:59:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,9 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end 
 
-
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name}. I am a big time programmer in #{language}."
+end 
 
 
 
